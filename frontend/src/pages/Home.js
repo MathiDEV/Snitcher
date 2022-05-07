1e0a8210a55425ea0ed04af3935e88b58d6a82ce
Dam <zaouak.adam@gmail.com>
2022-05-07 12:54:50 +0200

    :zap: Réparation de l'érreur de Mathias

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Navbar from '../components/Navbar';
+
+
+function Home()
+{
+
+    return (
+        <Navbar />
+    )
+}
+
+export default Home;
