62c8026de9b0e5549b25163d0075c152445e2d2a
Dam <zaouak.adam@gmail.com>
2022-05-07 11:05:00 +0200

    :zap: Init du frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
