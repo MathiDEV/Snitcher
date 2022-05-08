511f3532c8c72c9631b61aa7813722aec9087268
Mazaud Thomas <thomas.mazaud@epitech.eu>
2022-05-08 15:44:45 +0200

    [FIX] SQL Schema

--- snitcher.sql
+++ snitcher.sql
@@ -2,10 +2,10 @@
 -- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le : sam. 07 mai 2022 à 10:27
--- Version du serveur : 10.4.24-MariaDB
--- Version de PHP : 8.1.4
+-- Hôte : localhost:3306
+-- Généré le : dim. 08 mai 2022 à 13:44
+-- Version du serveur : 10.3.34-MariaDB-0ubuntu0.20.04.1
+-- Version de PHP : 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,14 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `automations`
+--
+
+CREATE TABLE `automations` (
+                               `id` int(10) UNSIGNED NOT NULL,
+                               `id_addr` int(10) UNSIGNED NOT NULL,
+                               `id_watcher` text DEFAULT NULL,
+                               `options` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `savelater`
+--
+
+CREATE TABLE `savelater` (
+                             `id` int(10) UNSIGNED NOT NULL,
+                             `id_addr` int(10) UNSIGNED NOT NULL,
+                             `save_addr` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `wallets`
 --
 
 CREATE TABLE `wallets` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `username` text NOT NULL,
-  `wallet` text NOT NULL,
-  `pro` tinyint(1) NOT NULL DEFAULT 0
+                           `id` int(10) UNSIGNED NOT NULL,
+                           `username` text DEFAULT NULL,
+                           `wallet` text NOT NULL,
+                           `nonce` text NOT NULL,
+                           `pro` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -41,9 +67,9 @@ CREATE TABLE `wallets` (
 --
 
 CREATE TABLE `watchers` (
-  `id` int(11) NOT NULL,
-  `wallet` int(10) UNSIGNED NOT NULL,
-  `action` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`action`))
+                            `id` int(11) NOT NULL,
+                            `wallet` int(10) UNSIGNED NOT NULL,
+                            `action` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`action`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -51,37 +77,62 @@ CREATE TABLE `watchers` (
 --
 
 --
+-- Index pour la table `automations`
+--
+ALTER TABLE `automations`
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `id_addr` (`id_addr`);
+
+--
+-- Index pour la table `savelater`
+--
+ALTER TABLE `savelater`
+    ADD PRIMARY KEY (`id`),
+    ADD KEY `id` (`id_addr`);
+
+--
 -- Index pour la table `wallets`
 --
 ALTER TABLE `wallets`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `wallet` (`wallet`) USING HASH;
-
---
--- Index pour la table `watchers`
---
-ALTER TABLE `watchers`
-  ADD KEY `wallet` (`wallet`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `automations`
+--
+ALTER TABLE `automations`
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `savelater`
+--
+ALTER TABLE `savelater`
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT pour la table `wallets`
 --
 ALTER TABLE `wallets`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+    MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `watchers`
+-- Contraintes pour la table `automations`
 --
-ALTER TABLE `watchers`
-  ADD CONSTRAINT `wallet` FOREIGN KEY (`wallet`) REFERENCES `wallets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `automations`
+    ADD CONSTRAINT `id_addr` FOREIGN KEY (`id_addr`) REFERENCES `wallets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `savelater`
+--
+ALTER TABLE `savelater`
+    ADD CONSTRAINT `id` FOREIGN KEY (`id_addr`) REFERENCES `wallets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
