dabbe32f3fc7e02659d587d2d176a56e88314840
Mathias A <mathias.andre@epitech.eu>
2022-05-08 03:03:41 +0200

    [OTHER] Updated route for best users.

--- Backend/api/user/bestUsers.js
+++ Backend/api/user/bestUsers.js
@@ -8,27 +8,34 @@ const bestUsers = (req, res) => {
             const rows = body.split('<tr>');
             rows.shift();
             const columns = rows.map(row => {
-                    const columns = row.split('<td>');
-                    columns.shift();
-                    return columns.map(column => {
-                            let value = column.split('</td>')[0];
-                            value = value.replace(/<\/?[^>]+(>|$)/g, "");
-                            return value;
-                        }
-                    );
+                const columns = row.split('<td>');
+                columns.shift();
+                return columns.map(column => {
+                    let value = column.split('</td>')[0];
+                    value = value.replace(/<\/?[^>]+(>|$)/g, "");
+                    return value;
                 }
+                );
+            }
             );
             const users = columns.map(column => {
+                let balance = column[3].replace(" Ether", "").replace(/,/g, "");
+                    rawBalance = parseInt(balance.replace(".", ""));
+                rawBalance *= Math.pow(10, 16 - rawBalance.toString().length);
                 return {
-                    rank: column[0],
+                    network: "ethereum-mainnet",
                     address: column[1],
-                    balance: column[3].replace(" Ether", ""),
+                    balance: {
+                        hex: '0x' + rawBalance.toString(16),
+                        formatted: balance
+                    },
+                    currencySymbol: "ETH"
                 };
             });
-            res.status(200).json(users)
+            res.status(200).json(users.slice(0, 10))
         })
         .catch(err => {
-            res.status(500).json({"msg": "Internal server error"})
+            res.status(500).json({ "msg": "Internal server error" })
         })
 }
 
